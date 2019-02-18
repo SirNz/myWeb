8a0d464019d74686304d14a63f44c8152ba1e4ed
Key <jerrynz@163.com>
2019-02-18 20:54:38 +0800

    日常更新

---
+++ PHP/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    Welcome to my personal website!<br>
+    Your username is <font color="red"><b><?php echo $_POST["username"]; ?></b></font><br>
+    Your password is <font color="red"><b><?php echo $_POST["password"]; ?></b></font>
+
+</body>
+</html>
