1a4907f3cca94fe9903d3656e00f21b9cd377b06
YW-Evi <jerrynz@163.com>
2021-05-25 14:27:43 +0800

    test commit

--- js/homepage.js
+++ js/homepage.js
@@ -1,4 +1,12 @@
+import { createPopper } from '@popperjs/core'
+
+const popconrn = document.querySelector('#popcorn');
+const tooltip = document.querySelector('#tooltip');
+
+createPopper(popconrn, tooltip, {
+    placement: 'bottom-end' ,
+})
+
 function getArticleCount(){
-    var e;
-    
+    var e;   
 }
